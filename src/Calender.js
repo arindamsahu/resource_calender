a1026fd678beb39ad9f43548019e2086c8459522
arindamsahu <arindam_sahu@ymail.com>
2018-05-23 14:27:48 -0500

    Crash test

--- src/Calender.js
+++ src/Calender.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 import moment from 'moment'
 import 'fullcalendar';
 import 'fullcalendar-scheduler';
+import { server } from './config';
 
 class Calender extends Component {
 
@@ -12,61 +13,68 @@ class Calender extends Component {
       users: [],
       events: []
     };
+    // binding this keywork with _selectCallBack scope
     this._selectCallBack = this._selectCallBack.bind(this);
   }
 
-  componentDidMount(){
+// fetch the user and event data.
+componentDidMount(){
 
-    let currentComponent = this;
+  let currentComponent = this;
 
-    let userData = fetch("/users.json")
-    .then(response => response.json());
+  let userData = fetch(`${server}users.json`)
+  .then(response => response.json());
 
-    let eventData = fetch("/event.json")
-    .then(response => response.json());
+  let eventData = fetch(`${server}event.json`)
+  .then(response => response.json());
+
+    // mutually exclusive data
 
     Promise.all([userData,eventData]).then(function(values){
 
       let users = [], events = [];
       let task = "green", meeting="orange";
 
-       values[0].result.map(obj1 => {
-          let user = {};
-          user.id = obj1.user_id;
-          user.title = obj1.name;
-          users.push(user);
-       });
+      values[0].result.map(obj1 => {
+        let user = {};
+        user.id = obj1.user_id;
+        user.title = obj1.name;
+        users.push(user);
+      });
 
-        values[1].result.map(obj2 => {
-          let event = {};
-          event.id = obj2.event_id;
-          event.end =  moment(obj2.end_date_time, 'YYYYMMDDTHHmmss').format('YYYY-MM-DDTHH:mm:ss');
-          event.start =  moment(obj2.start_date_time,'YYYYMMDDTHHmmss').format('YYYY-MM-DDTHH:mm:ss');
-          event.title = obj2.name;
-          event.type = obj2.type;
+      values[1].result.map(obj2 => {
+        let event = {};
+        event.id = obj2.event_id;
+        event.end =  moment(obj2.end_date_time, 'YYYYMMDDTHHmmss');
+        event.start =  moment(obj2.start_date_time,'YYYYMMDDTHHmmss');
+        event.title = obj2.name;
+        event.type = obj2.type;
 
-          users.map(user => {
-            if(user.id === obj2.user){
-              event.resourceId = obj2.user;
+        users.map(user => {
+          if(user.id === obj2.user){
+            event.resourceId = obj2.user;
             if(obj2.type === "task"){
               user.eventColor = task;
             }
             else if(obj2.type === "meeting"){
               user.eventColor = meeting;
-          }
             }
-          });
+          }
+        });
 
-          events.push(event);
-       });
-
+        events.push(event);
+      });
 
       currentComponent.setState({users: users, events: events});
+    }).catch(function(err){
+      console.error('err', err);
+      window.alert(err);
     });
 
   }
 
   _selectCallBack(start, end, jsEvent, view, resource){
+
     let event = {};
 
     event.start = moment(start,'YYYY-MM-DDTHH:mm:ss');
@@ -77,36 +85,37 @@ class Calender extends Component {
     
     this.state.events.push(event);
 
-    $('#calendar').fullCalendar('destroy');
-    this._loadFullCalender();
+  // destrying old calender instance for new state to take effect
+  $('#calendar').fullCalendar('destroy');
+  this._loadFullCalender();
 
-  }
+}
 
-  _loadFullCalender(){
-    $('#calendar').fullCalendar({
-      now: '2018-05-08',
-      selectable: true,
-      selectConstraint : "businessHours",
-      height: 400,
-      defaultView: 'timelineDay',
-      resourceLabelText: 'Resources',
-      resources: this.state.users,
-      events: this.state.events,
-      schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
-      businessHours: true,
-      eventOverlap: false,
-      select : this._selectCallBack
+_loadFullCalender(){
+  $('#calendar').fullCalendar({
+    now: '2018-05-08',
+    selectable: true,
+    selectConstraint : "businessHours",
+    height: 400,
+    defaultView: 'timelineDay',
+    resourceLabelText: 'Resources',
+    resources: this.state.users,
+    events: this.state.events,
+    schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
+    businessHours: true,
+    eventOverlap: false,
+    select : this._selectCallBack
   })
-  }
+}
 
-  render() {
-    return (
-      <div>
-      <div id='calendar'></div>
-        {this._loadFullCalender()}
-      </div>
-      );
-  }
+render() {
+  return (
+    <div>
+    <div id='calendar'></div>
+    {this._loadFullCalender()}
+    </div>
+    );
+}
 }
 
 export default Calender;
--- src/Calender.test.js
+++ src/Calender.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Calender from './Calender'
+import fetch from 'isomorphic-fetch';
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Calender/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
